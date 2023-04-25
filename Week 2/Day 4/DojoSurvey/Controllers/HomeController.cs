f44ea5c81456d893757b4c8969baae95800d4621
oussama0710 <ouxavi80@gmail.com>
2023-04-25 14:52:43 +0100

    Dojo Survey

---
+++ Week 2/Day 4/DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost("survey")]
+    public IActionResult Submission(Survey yourSurvey)
+    {
+        ViewBag.yourSurvey = yourSurvey;
+        return View("Result");
+    }
+
+
+
+
+
+
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Week 2/Day 4/DojoSurvey/Models/SurveyModel.cs
@@ -0,0 +1,8 @@
+#pragma warning disable CS8618
+public class Survey
+{
+    public string Name { get; set; }
+    public string DojoLocation { get; set; }
+    public string FavouriteLanguage { get; set; }
+    public string Comment { get; set; }
+}
