87ee010c4559105c7f53f06294fe57ff1e3b3f4c
oussama0710 <ouxavi80@gmail.com>
2023-04-26 23:14:34 +0100

    CRUDelicious

--- Week 3/Day 2/CRUDelicious/Controllers/HomeController.cs
+++ Week 3/Day 2/CRUDelicious/Controllers/HomeController.cs
@@ -1,25 +1,58 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CRUDelicious.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUDelicious.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private MyContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, MyContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        List<Dish> AllDishes = _context.Dishes.ToList();
+        ViewBag.AllDishes=AllDishes;
+        return View();
+    }
+    
+    [HttpPost(template: "/Dishes/create")]
+    public IActionResult CreateDish(Dish newDish)
+    {
+        if(ModelState.IsValid)
+        {
+            // Add
+            _context.Add(newDish);
+            // Save
+            _context.SaveChanges();
+            // -Redirect
+            return RedirectToAction("Index");
+        }
+        return View("Index");
+    }
+    [HttpGet("/dishes/{DishId}")]
+    public IActionResult DishShow(int DishId)
+    {
+         List<Dish> AllDishes = _context.Dishes.ToList();
+        IEnumerable<Dish> OneDish = AllDishes.Where(dish => dish.DishId == DishId);
+        foreach (var One in OneDish)
+        {
+            ViewBag.OneDish=One;
+        }
+            
         return View();
     }
 
     public IActionResult Privacy()
     {
+
         return View();
     }
 
---
+++ Week 3/Day 2/CRUDelicious/Models/MyContext.cs
@@ -0,0 +1,12 @@
+#pragma warning disable CS8618
+
+
+using Microsoft.EntityFrameworkCore;
+namespace CRUDelicious.Models;
+
+public class MyContext : DbContext 
+{ 
+    public MyContext(DbContextOptions options) : base(options) { }
+    // Tables
+    public DbSet<Dish> Dishes {get;set;}
+}
